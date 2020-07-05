cc3de2907092f8f2d614804ad6d0c74639528777
hafdhisofien <1114@holbertonschool.com>
2020-07-05 15:14:29 +0000

    clean

--- web_dynamic/static/scripts/1-hbnb.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  let my_amenities = {};                                                                                                  $(document).on('change', "input[type='checkbox']", function () {
-    if (this.checked) {
-      my_amenities[$(this).data('id')] = $(this).data('name');                                                              } else {
-      delete my_amenities[$(this).data('id')];                                                                              }
-    let my_list = Object.values(my_amenities);                                                                              if (my_list.length > 0) {                                                                                                 $('div.amenities > h4').text(Object.values(my_amenities).join(', '));                                                 } else {
-      $('div.amenities > h4').html('&nbsp;');
-    }
-  });
